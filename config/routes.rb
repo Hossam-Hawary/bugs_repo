b105c05956166304bc4180731d97ac3563eacbe9
Hossam-Hawary <hossameldeen.hawary@gmail.com>
2017-06-09 06:33:17 +0200

    handle bugs index, show, create

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :states, :defaults => { :format => 'json' }
-  resources :bugs, :defaults => { :format => 'json' }
+  resources :bugs, only:[:index, :create,:update, :destroy ], :defaults => { :format => 'json' }
+  get "/bugs/:number", to: "bugs#show"
 end
