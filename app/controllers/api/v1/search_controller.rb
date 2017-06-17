f1ad888f073639ffd1c83995fd246662d7b2c59a
Hossam-Hawary <hossameldeen.hawary@gmail.com>
2017-06-17 14:03:07 +0200

    handle search errors

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -3,10 +3,15 @@ module Api::V1
   class SearchController < ApplicationController
     def search
       if params[:key].present? && params[:field].present?
-        bugs = Api::V1::Bug.search_by_field params[:token], params[:key], params[:field]
-        render :json => {success: true, status: 200, api_version:params[:controller], bugs:bugs.records.to_a.map! {|bug| bug.bug_as_json}}
+        begin
+          bugs = Api::V1::Bug.search_by_field params[:token], params[:key], params[:field]
+          bugs.records.to_a.map! {|bug| bug.bug_as_json}
+          render :json => {success: true, status: 200, api_version:params[:controller], bugs:bugs}
+        rescue  Elasticsearch::Transport::Transport::Errors::BadRequest
+              render :json => {success: false, status: 400, message:" Not Valid keys for search!",api_version:params[:controller]}
+        end
       else
-        render :json => {success: false, status: 400, message:" No Keyword or field Provided for search!",api_version:params[:controller], bugs:[] }
+        render :json => {success: false, status: 400, message:" No Keyword or field Provided for search!",api_version:params[:controller]}
       end
     end
 
